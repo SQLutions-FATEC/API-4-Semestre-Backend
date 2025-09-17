d64266c13b62d4221d23f170a4053ea444c7ce54
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2025-09-17 00:31:02 -0300

    feat: add initial database structure

---
+++ deploy/init.sql
@@ -0,0 +1,44 @@
+-- Criar tipo ENUM para veículos
+CREATE TYPE tipo_veiculo AS ENUM (
+    'Carro',
+    'Camionete',
+    'Ônibus',
+    'Van',
+    'Caminhão grande',
+    'Moto',
+    'Indefinido'
+);
+
+-- Endereço
+CREATE TABLE endereco (
+    id SERIAL PRIMARY KEY,
+    ende VARCHAR(150) NOT NULL UNIQUE
+);
+
+-- Radar
+CREATE TABLE radar (
+    id VARCHAR(9) PRIMARY KEY, -- camera_numero
+    id_end INT NOT NULL, --refere-se ao id do endereço
+    latitude DECIMAL(9,6) NOT NULL,
+    longitude DECIMAL(9,6) NOT NULL,
+    vel_reg INT NOT NULL,
+    CONSTRAINT fk_radar_endereco FOREIGN KEY (id_end) REFERENCES endereco(id)
+);
+
+-- Leitura
+CREATE TABLE leitura (
+    id SERIAL PRIMARY KEY,
+    id_rad VARCHAR(9) NOT NULL, --refere-se ao id do radar
+    dat_hora TIMESTAMP NOT NULL,
+    tip_vei tipo_veiculo NOT NULL,
+    vel INT NOT NULL,
+    CONSTRAINT fk_leitura_radar FOREIGN KEY (id_rad) REFERENCES radar(id)
+);
+
+-- Usuário
+CREATE TABLE usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL
+);
