2fa365bf53aaf79aa7f3b5f964725c68e86a6732
Cainã Melo <caina.melo@fatec.sp.gov.br>
2025-09-19 21:41:56 -0300

    feat(8): add User entity

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/entity/Usuario.java
@@ -0,0 +1,60 @@
+package com.sqlutions.api_4_semestre_backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usr_id")
+    private Long id;
+
+    @Column(name = "usr_nome")
+    private String nome;
+
+    @Column(name = "usr_email")
+    private String email;
+
+    @Column(name = "usr_senha")
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
