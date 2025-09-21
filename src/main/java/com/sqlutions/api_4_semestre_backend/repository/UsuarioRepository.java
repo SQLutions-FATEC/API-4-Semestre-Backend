2058e4a058725044606f81ed07c11700e7a86437
Cainã Melo <163151125+CainaNascimentoMelo@users.noreply.github.com>
2025-09-21 15:52:24 -0300

    feat(8): add functions User repository

--- src/main/java/com/sqlutions/api_4_semestre_backend/repository/UsuarioRepository.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.sqlutions.api_4_semestre_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.sqlutions.api_4_semestre_backend.entity.Usuario;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+    Optinal<Usuario> findByEmail(String email);
     
 }
+
