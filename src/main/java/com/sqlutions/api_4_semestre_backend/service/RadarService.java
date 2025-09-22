63128c0e884d3f6252c7751a5e4ac69aa0ec3c44
Joaovsnas <166765258+Joaovsnas@users.noreply.github.com>
2025-09-22 22:38:19 +0000

    feat (6) update on radar service

--- src/main/java/com/sqlutions/api_4_semestre_backend/service/RadarService.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/service/RadarService.java
@@ -1,5 +1,10 @@
 package com.sqlutions.api_4_semestre_backend.service;
 
 public interface RadarService {
-    
+    Radar createRadar(Radar radar);
+    List<Radar> getAllRadars();
+    Optional<Radar> getRadarById(String id);
+    Radar updateRadar(String id, Radar radar);
+    void deleteRadar(String id);
+    List<Radar> getRadarsByAddress(Long addressId);
 }
