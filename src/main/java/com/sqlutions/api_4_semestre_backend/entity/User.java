36d79215f5e8bfc530909c3f6017f247a5b4e0f0
Cainã Melo <caina.melo@fatec.sp.gov.br>
2025-09-22 21:28:04 -0300

    feat(8): update name entity User

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/entity/User.java
@@ -0,0 +1,73 @@
+package com.sqlutions.api_4_semestre_backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usr_id")
+    private Long id;
+
+    @Column(nullable = false, name = "usr_name")
+    private String name;
+
+    @Column(nullable = false, unique = true, name = "usr_email")
+    private String email;
+
+    @Column(nullable = false, name = "usr_password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "usr_role")
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+}
