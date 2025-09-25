f9f0a8e827d6b1bd37fdd637de946b8b325c42ef
Cainã Melo <caina.melo@fatec.sp.gov.br>
2025-09-24 22:02:23 -0300

    feat(31): add functions Adress repository

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/repository/AdressRepository.java
@@ -0,0 +1,9 @@
+package com.sqlutions.api_4_semestre_backend.repository;
+
+import com.sqlutions.api_4_semestre_backend.entity.Adress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdressRepository extends JpaRepository<Adress, Long> {
+}
+
+
