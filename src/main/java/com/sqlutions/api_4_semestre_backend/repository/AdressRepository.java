e46756503394085cd5620662df2604e564ce04ea
Cainã Melo <163151125+CainaNascimentoMelo@users.noreply.github.com>
2025-09-26 11:00:03 -0300

    delete(31): delete files wrong name

--- src/main/java/com/sqlutions/api_4_semestre_backend/repository/AdressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sqlutions.api_4_semestre_backend.repository;
-
-import com.sqlutions.api_4_semestre_backend.entity.Adress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdressRepository extends JpaRepository<Adress, Long> {
-}
-
-
