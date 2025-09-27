4fd5efd4997fcbba571c30620c10f2de5215e749
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2025-09-27 16:43:43 -0300

    feat: update IndexService to return 404s when no readings are found within criteria

--- src/main/java/com/sqlutions/api_4_semestre_backend/service/IndexServiceImpl.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/service/IndexServiceImpl.java
@@ -3,7 +3,9 @@ package com.sqlutions.api_4_semestre_backend.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sqlutions.api_4_semestre_backend.entity.Radar;
 import com.sqlutions.api_4_semestre_backend.entity.Reading;
@@ -16,20 +18,27 @@ public class IndexServiceImpl implements IndexService {
     private ReadingRepository readingRepository;
 
     /**
-     * Calcula o índice de segurança com base em uma lista de leituras de velocidade.
-     * O índice é um valor inteiro de 1 a 5, onde 1 representa a melhor segurança e 5 a pior.
+     * Calcula o índice de segurança com base em uma lista de leituras de
+     * velocidade.
+     * O índice é um valor inteiro de 1 a 5, onde 1 representa a melhor segurança e
+     * 5 a pior.
      * 
      * O cálculo considera dois fatores principais:
      * <ul>
-     *   <li>A porcentagem de veículos que excederam o limite de velocidade regulamentado.</li>
-     *   <li>A média do excesso de velocidade dos veículos que ultrapassaram o limite.</li>
+     * <li>A porcentagem de veículos que excederam o limite de velocidade
+     * regulamentado.</li>
+     * <li>A média do excesso de velocidade dos veículos que ultrapassaram o
+     * limite.</li>
      * </ul>
      * 
-     * Os fatores são ponderados (40% para a porcentagem de infrações e 60% para a média do excesso)
+     * Os fatores são ponderados (40% para a porcentagem de infrações e 60% para a
+     * média do excesso)
      * e combinados para gerar um índice normalizado entre 1 e 5.
      * 
-     * @param readings Lista de leituras de velocidade, cada uma contendo a velocidade registrada e o limite regulamentado.
-     * @return Índice de segurança calculado (1 a 5), onde valores menores indicam maior segurança.
+     * @param readings Lista de leituras de velocidade, cada uma contendo a
+     *                 velocidade registrada e o limite regulamentado.
+     * @return Índice de segurança calculado (1 a 5), onde valores menores indicam
+     *         maior segurança.
      */
     private Integer getSecurityIndex(List<Reading> readings) {
         // dada uma lista de leituras, calcular a porcentagem de carros que estão
@@ -37,7 +46,7 @@ public class IndexServiceImpl implements IndexService {
         Long totalReadings = (long) readings.size();
         System.out.println(totalReadings);
         if (totalReadings == 0) {
-            return 1; // se não houver leituras, considerar que o índice é o melhor possível
+            return 1;
         }
         Long nOverLimit = readings.stream().filter(r -> r.getSpeed() > r.getRadar().getRegulatedSpeed()).count();
         Float overLimitWeight = 0.4f; // peso da porcentagem de carros acima do limite
@@ -64,16 +73,23 @@ public class IndexServiceImpl implements IndexService {
     /**
      * Calcula o índice geral da cidade para um determinado intervalo de tempo.
      * <p>
-     * Esta função agrega diversos índices (por exemplo, tráfego, segurança) com base nas leituras
-     * coletadas entre {@code timestamp - minutes} e {@code timestamp}. O índice resultante
-     * representa o status geral da cidade, onde valores menores indicam melhores condições.
+     * Esta função agrega diversos índices (por exemplo, tráfego, segurança) com
+     * base nas leituras
+     * coletadas entre {@code timestamp - minutes} e {@code timestamp}. O índice
+     * resultante
+     * representa o status geral da cidade, onde valores menores indicam melhores
+     * condições.
      * <p>
-     * Atualmente, o cálculo é baseado no índice de segurança, determinado pela análise
-     * da porcentagem de veículos que excederam o limite de velocidade no período especificado.
+     * Atualmente, o cálculo é baseado no índice de segurança, determinado pela
+     * análise
+     * da porcentagem de veículos que excederam o limite de velocidade no período
+     * especificado.
      * 
-     * @param minutes   número de minutos a considerar a partir do timestamp informado
+     * @param minutes   número de minutos a considerar a partir do timestamp
+     *                  informado
      * @param timestamp fim do intervalo de tempo para o cálculo do índice
-     * @return valor inteiro representando o índice da cidade (1 a 5, onde menor é melhor)
+     * @return valor inteiro representando o índice da cidade (1 a 5, onde menor é
+     *         melhor)
      */
     public Integer getCityIndex(int minutes, java.time.LocalDateTime timestamp) {
         // TODO: adicionar outros índices
@@ -94,6 +110,10 @@ public class IndexServiceImpl implements IndexService {
         System.out.println("Calculating radar index for time range: " + timeStart + " to " + timeEnd);
         List<Reading> readings = readingRepository.findByRadarInAndDateBetween(List.of(radars), timeStart, timeEnd);
         System.out.println("Reading count: " + readings.size());
+        if (readings.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "No readings found for the specified time range and radars");
+        }
         Integer securityIndex = getSecurityIndex(readings);
 
         return securityIndex;
@@ -104,8 +124,13 @@ public class IndexServiceImpl implements IndexService {
         java.time.LocalDateTime timeEnd = timestamp;
         java.time.LocalDateTime timeStart = timeEnd.minusMinutes(minutes);
         System.out.println("Calculating region index for time range: " + timeStart + " to " + timeEnd);
-        List<Reading> readings = readingRepository.findByRadarAddressRegionInAndDateBetween(List.of(region), timeStart, timeEnd);
+        List<Reading> readings = readingRepository.findByRadarAddressRegionInAndDateBetween(List.of(region), timeStart,
+                timeEnd);
         System.out.println("Reading count: " + readings.size());
+        if (readings.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "No readings found for the specified time range and region");
+        }
         Integer securityIndex = getSecurityIndex(readings);
 
         return securityIndex;
