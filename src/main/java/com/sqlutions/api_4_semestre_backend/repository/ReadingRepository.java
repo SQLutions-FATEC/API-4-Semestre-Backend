1c5545ced8d9ae21d9bd53ac9af5099ab42f4851
Cainã Melo <163151125+CainaNascimentoMelo@users.noreply.github.com>
2025-10-15 22:28:25 -0300

    remove(38): remove unused import

--- src/main/java/com/sqlutions/api_4_semestre_backend/repository/ReadingRepository.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/repository/ReadingRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.sqlutions.api_4_semestre_backend.entity.Address;
 import com.sqlutions.api_4_semestre_backend.entity.Radar;
 import com.sqlutions.api_4_semestre_backend.entity.Reading;
 
