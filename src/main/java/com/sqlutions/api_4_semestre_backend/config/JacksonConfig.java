7b11868fa30178f8698babee2b77ec1f5c352315
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2025-10-16 14:23:46 -0300

    fix: Jackson wrong time formatting

--- src/main/java/com/sqlutions/api_4_semestre_backend/config/JacksonConfig.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/config/JacksonConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.locationtech.jts.geom.Geometry;
 
 @Configuration
@@ -13,9 +15,16 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        
+        // Register Java 8 Date/Time module
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        
+        // Register custom Geometry serializer
         SimpleModule module = new SimpleModule();
         module.addSerializer(Geometry.class, new GeometrySerializer());
         mapper.registerModule(module);
+        
         return mapper;
     }
 }
