5e76649bbf425223fb5015b5ba50178034b99131
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-10-20 17:24:33 -0300

    Feat(35):Add BustopService.java

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/service/BusStopService.java
@@ -0,0 +1,24 @@
+package com.sqlutions.api_4_semestre_backend.service;
+
+import java.util.List;
+
+import com.sqlutions.api_4_semestre_backend.entity.BusStop;
+
+public interface BusStopService {
+
+    // Read (Todos)
+    List<BusStop> listBusStops();
+
+    // Read (Por ID)
+    BusStop searchBusStopById(Long id);
+
+    // Create / Update
+    // Se o ID for null ou não existir, cria; se existir, atualiza.
+    BusStop saveBusStop(BusStop busStop);
+
+    // Delete
+    void deleteBusStop(Long id);
+
+    // (Opcional, mas útil) Você pode adicionar aqui métodos espaciais
+    // Por exemplo: List<PontoOnibus> findPointsInRegion(Long regiaoId);
+}
