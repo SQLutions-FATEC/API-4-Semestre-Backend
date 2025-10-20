51498db38c847ace1d3b24b81777850ca6df1be3
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-10-20 16:29:32 -0300

    Feat(35):add regions entity

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/entity/Regions.java
@@ -0,0 +1,54 @@
+package com.sqlutions.api_4_semestre_backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.locationtech.jts.geom.Polygon; 
+
+@Entity
+@Table(name = "regioes")
+public class Regions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_regiao", length = 100, nullable = false, unique = true)
+    private String nomeRegiao;
+
+    // Mapeamento da GEOMETRY(Polygon, 4326)
+    @Column(name = "area_regiao", columnDefinition = "geometry(Polygon, 4326)", nullable = false)
+    private Polygon areaRegiao;
+
+    // Construtores
+    public Regions() {
+    }
+
+    // --- Getters e Setters ---
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeRegiao() {
+        return nomeRegiao;
+    }
+
+    public void setNomeRegiao(String nomeRegiao) {
+        this.nomeRegiao = nomeRegiao;
+    }
+
+    public Polygon getAreaRegiao() {
+        return areaRegiao;
+    }
+
+    public void setAreaRegiao(Polygon areaRegiao) {
+        this.areaRegiao = areaRegiao;
+    }
+}
