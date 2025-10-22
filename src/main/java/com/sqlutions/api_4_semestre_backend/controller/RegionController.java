4fd58d1578b98326f1326d13d14d48433b22ee08
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-10-22 20:55:40 -0300

    Fix(35): Fix cors error

--- src/main/java/com/sqlutions/api_4_semestre_backend/controller/RegionController.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/controller/RegionController.java
@@ -3,6 +3,7 @@ package com.sqlutions.api_4_semestre_backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.sqlutions.api_4_semestre_backend.service.TimeService;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/regions")
 public class RegionController {
 
