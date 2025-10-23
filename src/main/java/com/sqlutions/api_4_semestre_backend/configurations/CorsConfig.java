e31b083f0e049b317a8eafbb4d3e9f2e0afb361d
EnzoLFranco <fenzolemos@gmail.com>
2025-10-23 17:11:08 -0300

    fix(13): update allowed origins in CorsConfig to include localhost:80

--- src/main/java/com/sqlutions/api_4_semestre_backend/configurations/CorsConfig.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/configurations/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",
-                        "http://localhost:5173")
+                        "http://localhost:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
