713a3720c653f35d1ffe55a62e99877bca4a96f1
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-10-23 19:14:01 -0300

    Fix(35): FIx RequestParam in controller

--- src/main/java/com/sqlutions/api_4_semestre_backend/controller/IndexController.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/controller/IndexController.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sqlutions.api_4_semestre_backend.entity.Index;
-import com.sqlutions.api_4_semestre_backend.entity.Radar;
 import com.sqlutions.api_4_semestre_backend.service.IndexService;
 import com.sqlutions.api_4_semestre_backend.service.TimeService;
 
@@ -30,14 +28,14 @@ public class IndexController {
     }
 
     @GetMapping("/radar")
-    public Index getRadarIndexes(@RequestParam(defaultValue = "1") int minutes, @RequestBody Radar[] radars,
+    public Index getRadarIndexes(@RequestParam(defaultValue = "1") int minutes, @RequestParam String[] radars,
             @RequestParam(required = false) java.time.LocalDateTime timestamp) {
         return indexService.getRadarIndexes(minutes, radars,
                 timestamp == null ? timeService.getCurrentTimeClampedToDatabase() : timestamp);
     }
 
     @GetMapping("/region")
-    public Index getRegionIndex(@RequestParam(defaultValue = "1") int minutes, @RequestBody String region,
+    public Index getRegionIndex(@RequestParam(defaultValue = "1") int minutes, @RequestParam String region,
             @RequestParam(required = false) java.time.LocalDateTime timestamp) {
         return indexService.getRegionIndex(minutes, region,
                 timestamp == null ? timeService.getCurrentTimeClampedToDatabase() : timestamp);
@@ -51,14 +49,14 @@ public class IndexController {
     }
 
     @GetMapping("/radar/series")
-    public List<Index> getRadarIndexesSeries(@RequestParam(defaultValue = "1") int minutes, @RequestBody Radar[] radars,
+    public List<Index> getRadarIndexesSeries(@RequestParam(defaultValue = "1") int minutes, @RequestParam String[] radars,
             @RequestParam(required = false) java.time.LocalDateTime timestamp) {
         return indexService.getRadarIndexesSeries(minutes, radars,
                 timestamp == null ? timeService.getCurrentTimeClampedToDatabase() : timestamp);
     }
 
     @GetMapping("/region/series")
-    public List<Index> getRegionIndexSeries(@RequestParam(defaultValue = "1") int minutes, @RequestBody String region,
+    public List<Index> getRegionIndexSeries(@RequestParam(defaultValue = "1") int minutes, @RequestParam String region,
             @RequestParam(required = false) java.time.LocalDateTime timestamp) {
         return indexService.getRegionIndexSeries(minutes, region,
                 timestamp == null ? timeService.getCurrentTimeClampedToDatabase() : timestamp);
