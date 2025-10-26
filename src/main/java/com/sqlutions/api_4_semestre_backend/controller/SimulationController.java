f5cb53a69957804e8859955f2e14f77f99028a5d
Joaovsnas <166765258+Joaovsnas@users.noreply.github.com>
2025-10-26 21:03:36 +0000

    feat(SCRUM-40): Creation of simulationcontroller

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/controller/SimulationController.java
@@ -0,0 +1,39 @@
+package com.sqlutions.api_4_semestre_backend.controller;
+
+import com.sqlutions.api_4_semestre_backend.entity.Index;
+import com.sqlutions.api_4_semestre_backend.event.HighIndexEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/simulate")
+public class SimulationController {
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    public SimulationController(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
+    @GetMapping
+    public String simulate(
+            @RequestParam(defaultValue = "0") int trafficIndex,
+            @RequestParam(defaultValue = "0") int securityIndex
+    ) {
+        Index index = new Index(trafficIndex, securityIndex);
+
+        eventPublisher.publishEvent(new HighIndexEvent(this, index));
+
+        return String.format(
+            " Simulação enviada! (Tráfego: %d, Segurança: %d)",
+            trafficIndex, securityIndex
+        );
+    }
+
+    @GetMapping("/all")
+    public String simulateAll() {
+        Index index = new Index(5, 5);
+        eventPublisher.publishEvent(new HighIndexEvent(this, index));
+        return " Simulação completa enviada (tráfego e segurança críticos)!";
+    }
+}
