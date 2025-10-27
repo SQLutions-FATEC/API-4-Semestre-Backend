1ef346aea329c8dd1a247597af38f46475ff286f
Joaovsnas <166765258+Joaovsnas@users.noreply.github.com>
2025-10-27 04:04:21 -0300

    feat(SCRUM-41): highindexeventlistener

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/listener/HighIndexEventListener.java
@@ -0,0 +1,56 @@
+package com.sqlutions.api_4_semestre_backend.listener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.sqlutions.api_4_semestre_backend.event.HighIndexEvent;
+import com.sqlutions.api_4_semestre_backend.service.NotificationService;
+
+@Component
+public class HighIndexEventListener {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    @EventListener
+    public void handleHighIndexEvent(HighIndexEvent event) {
+        var index = event.getIndex();
+
+        int trafficIndex = index.getTrafficIndex();
+        int securityIndex = index.getSecurityIndex();
+        String timestamp = LocalDateTime.now().minusHours(3).format(formatter);
+
+
+        if (trafficIndex >= 3) {
+            String message = String.format(
+                "🚦 *ALERTA DE NÍVEL CRÍTICO – TRÁFEGO* 🚦\n\n" +
+                "🔹 Nível de Tráfego: *%d*\n" +
+                "⚠️ Condições de congestionamento elevadas foram detectadas.\n\n" +
+                "📅 Data/hora: %s",
+                trafficIndex, timestamp
+            );
+
+            System.out.println(" Enviando alerta sobre tráfego...");
+            notificationService.sendAlert(message, "TRAFFIC", trafficIndex);
+        }
+
+        if (securityIndex >= 3) {
+            String message = String.format(
+                "🚨 *ALERTA DE NÍVEL CRÍTICO – SEGURANÇA* 🚨\n\n" +
+                "🔹 Nível de Segurança: *%d*\n" +
+                "⚠️ Atenção: condições inseguras detectadas na via.\n\n" +
+                "📅 Data/hora: %s",
+                securityIndex, timestamp
+            );
+
+            System.out.println(" Enviando alerta sobre segurança...");
+            notificationService.sendAlert(message, "SECURITY", securityIndex);
+        }
+    }
+}
