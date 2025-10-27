b1be821ce6ff54b96f3976e14edc6eac08fdc142
Joaovsnas <166765258+Joaovsnas@users.noreply.github.com>
2025-10-27 20:04:47 -0300

    feat(41): add scheduling to application

--- src/main/java/com/sqlutions/api_4_semestre_backend/Api4SemestreBackendApplication.java
+++ src/main/java/com/sqlutions/api_4_semestre_backend/Api4SemestreBackendApplication.java
@@ -2,7 +2,9 @@ package com.sqlutions.api_4_semestre_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Api4SemestreBackendApplication {
 
