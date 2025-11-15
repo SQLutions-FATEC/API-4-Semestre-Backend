efc832e54f4df20a352f45d94ef8473115494501
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2025-11-15 12:30:59 -0300

    feat(69): add new dto for informations in heatmap

---
+++ src/main/java/com/sqlutions/api_4_semestre_backend/dto/AddressHeatMap.java
@@ -0,0 +1,61 @@
+package com.sqlutions.api_4_semestre_backend.dto;
+
+
+public class AddressHeatMap {
+    
+    private String nomeEndereco;
+    private String areaRuaGeoJson;
+    private Integer trafficIndex;
+    private Integer securityIndex;
+    private Integer overallIndex;
+
+
+    public AddressHeatMap(String nomeEndereco, String areaRuaGeoJson, Integer trafficIndex, Integer securityIndex, Integer overallIndex) {
+        this.nomeEndereco = nomeEndereco;
+        this.areaRuaGeoJson = areaRuaGeoJson;
+        this.trafficIndex = trafficIndex;
+        this.securityIndex = securityIndex;
+        this.overallIndex = overallIndex;
+    }
+
+    public String getNomeEndereco() {
+        return nomeEndereco;
+    }
+
+    public void setNomeEndereco(String nomeEndereco) {
+        this.nomeEndereco = nomeEndereco;
+    }
+
+    public String getAreaRuaGeoJson() {
+        return areaRuaGeoJson;
+    }
+
+    public void setAreaRuaGeoJson(String areaRuaGeoJson) {
+        this.areaRuaGeoJson = areaRuaGeoJson;
+    }
+
+    public Integer getTrafficIndex() {
+        return trafficIndex;
+    }
+
+    public void setTrafficIndex(Integer trafficIndex) {
+        this.trafficIndex = trafficIndex;
+    }
+
+    public Integer getSecurityIndex() {
+        return securityIndex;
+    }
+
+    public void setSecurityIndex(Integer securityIndex) {
+        this.securityIndex = securityIndex;
+    }
+
+    public Integer getOverallIndex() {
+        return overallIndex;
+    }
+
+    public void setOverallIndex(Integer overallIndex) {
+        this.overallIndex = overallIndex;
+    }
+
+}
