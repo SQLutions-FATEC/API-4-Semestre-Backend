fb37535d7aa7601fc6e2058c642e8c9411bdd5a7
BryanARMatheus <bryanassis244@gmail.com>
2025-12-01 00:31:45 -0300

    fix(80): removes : from log_notificacao table

--- deploy/postgres/init.sql
+++ deploy/postgres/init.sql
@@ -1,4 +1,4 @@
-\c api;
+\ c api;
 -- Criar tipo ENUM para veículos
 CREATE TYPE tipo_veiculo AS ENUM (
     'Carro',
@@ -9,11 +9,7 @@ CREATE TYPE tipo_veiculo AS ENUM (
     'Moto',
     'Indefinido'
 );
-CREATE TYPE nivel_usuario AS ENUM (
-    'Admin', 
-    'Gestor'
-);
-
+CREATE TYPE nivel_usuario AS ENUM ('Admin', 'Gestor');
 -- Endereço
 CREATE TABLE endereco (
     id SERIAL PRIMARY KEY,
@@ -22,28 +18,28 @@ CREATE TABLE endereco (
     regiao VARCHAR(30),
     trecho GEOMETRY(LineString, 4326)
 );
-
 -- Radar
 CREATE TABLE radar (
-    id VARCHAR(9) PRIMARY KEY, -- camera_numero
-    id_end INT NOT NULL, --refere-se ao id do endereço
+    id VARCHAR(9) PRIMARY KEY,
+    -- camera_numero
+    id_end INT NOT NULL,
+    --refere-se ao id do endereço
     localizacao GEOMETRY(Point, 4326),
     vel_reg INT NOT NULL,
     carros_min_med NUMERIC(10, 2),
     carros_min_max NUMERIC(10, 2),
     CONSTRAINT fk_radar_endereco FOREIGN KEY (id_end) REFERENCES endereco(id)
 );
-
 -- Leitura
 CREATE TABLE leitura (
     id SERIAL PRIMARY KEY,
-    id_rad VARCHAR(9) NOT NULL, --refere-se ao id do radar
+    id_rad VARCHAR(9) NOT NULL,
+    --refere-se ao id do radar
     dat_hora TIMESTAMP NOT NULL,
     tip_vei tipo_veiculo NOT NULL,
     vel INT NOT NULL,
     CONSTRAINT fk_leitura_radar FOREIGN KEY (id_rad) REFERENCES radar(id)
 );
-
 -- Usuário
 CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
@@ -52,28 +48,24 @@ CREATE TABLE usuario (
     senha VARCHAR(100) NOT NULL,
     nivel nivel_usuario NOT NULL
 );
-
 -- Log de Notificações
 CREATE TABLE log_notificacao (
     id SERIAL PRIMARY KEY,
     id_usuario INT,
     mensagem TEXT NOT NULL,
     texto_relatorio TEXT,
-    tipo_indice: VARCHAR(20) NOT NULL,
+    tipo_indice VARCHAR(20) NOT NULL,
     valor_indice INT NOT NULL,
     data_emissao TIMESTAMP DEFAULT NOW(),
     data_conclusao TIMESTAMP,
     CONSTRAINT fk_log_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id) ON DELETE CASCADE
 );
-
-
 -- Regiões
 CREATE TABLE regioes (
     id SERIAL PRIMARY KEY,
     nome_regiao VARCHAR(100) NOT NULL UNIQUE,
     area_regiao GEOMETRY(Polygon, 4326) NOT NULL
 );
-
 -- Pontos de onibus
 CREATE TABLE pontos_onibus(
     id BIGINT PRIMARY KEY,
